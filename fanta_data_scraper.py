a309236de415e2e6ba16e5103746b6fcd503789a
fdp <81747109+fdp7@users.noreply.github.com>
2022-08-30 17:24:42 +0200

    better version

--- fanta_data_scraper.py
+++ fanta_data_scraper.py
@@ -1,3 +1,7 @@
+from itertools import cycle
+from random import randint
+from re import T
+from time import sleep
 import numpy as np
 import pandas as pd
 import requests
@@ -5,7 +9,6 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import os
 from termcolor import colored
-from requests_ip_rotator import ApiGateway, EXTRA_REGIONS
 
 def data_scraping(team, matchweek, season):
     
@@ -28,96 +31,88 @@ def data_scraping(team, matchweek, season):
     team_report = pd.DataFrame({'Names':players,'Grades':grades})
 
     # rename season and matchweek
-    #season = season.replace('/','-').split()
+    season = season.replace('/','-').split()
     matchweek = matchweek.split()
 
     # export to csv
-    filepath = str(season) + "/" + str(matchweek[1]) + "/"
+    filepath = str(season[1]) + "/" + str(matchweek[1]) + "/"
     os.makedirs(filepath, exist_ok=True) 
     filename = filepath + team + '.csv'
     team_report.to_csv(filename)
 
-
-# Start from first page
-page = requests.get('https://www.fantacalcio.it/pagelle/2021-22/atalanta/1')
-
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
-
 # Get seasons
-seasons_list = soup.find(class_='filters mt-4 mb-2')
-seasons_list = seasons_list.find(attrs={'name':'season'})
-seasons_list = seasons_list.find_all('option')
+def get_seasons():
+    
+    url = 'https://www.fantacalcio.it/pagelle'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    seasons_list = soup.find(class_='filters mt-4 mb-2')
+    seasons_list = seasons_list.find(attrs={'name':'season'})
+    seasons_list = seasons_list.find_all('option')
 
-seasons = np.array('Seasons')
-for season in seasons_list:
-    seasons = np.append(seasons,season)
+    seasons = np.array('Seasons')
+    for season in seasons_list:
+        seasons = np.append(seasons,season)
+    seasons = np.sort(seasons)[::-1]
+    return seasons
 
 # Get matchweeks
-matchweeks_list = soup.find(class_='filters mt-4 mb-2')
-matchweeks_list = matchweeks_list.find(attrs={'name':'matchweek'})
-matchweeks_list = matchweeks_list.find_all('option')
+def get_matchweeks():
+    
+    url = 'https://www.fantacalcio.it/pagelle'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-matchweeks = np.array('MatchWeek')
-for matchweek in matchweeks_list:
-    matchweeks = np.append(matchweeks,matchweek)
+    matchweeks_list = soup.find(class_='filters mt-4 mb-2')
+    matchweeks_list = matchweeks_list.find(attrs={'name':'matchweek'})
+    matchweeks_list = matchweeks_list.find_all('option')
+
+    matchweeks = np.array('MatchWeek')
+    for matchweek in matchweeks_list:
+        matchweeks = np.append(matchweeks,matchweek)
+    return matchweeks
 
 # Get teams
-teams_list = soup.find(class_='filters mt-4 mb-2')
-teams_list = teams_list.find(attrs={'name':'team-name'})
-teams_list = teams_list.find_all('option')
+def get_teams(season):
+    season = season.replace('/','-').split()
+    page = requests.get('https://www.fantacalcio.it/pagelle/' + str(season[1]) + '/atalanta/1')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    teams_list = soup.find(class_='filters mt-4 mb-2')
+    teams_list = teams_list.find(attrs={'name':'team-name'})
+    teams_list = teams_list.find_all('option')
 
-teams = np.array('Teams')
-for team in teams_list:
-    teams = np.append(teams,team)
+    teams = np.array('Teams')
+    for team in teams_list:
+        teams = np.append(teams,team)
+    return teams
 
-## Data Scraping for seasons,matchweek,teams
-#for season in seasons[1:len(seasons)-2]:  
-#    for matchweek in matchweeks[1:]:
-#        for team in teams[1:]:
-#        #team = random.choice(teams)
-#        #teams = np.delete(teams,np.where(teams == team))
-#            
-#            # rename season and matchweek
-#            s = season.replace('/','-').split()
-#            m = matchweek.split()
-#
-#            # shuffle headers 
-#            ua = UserAgent()
-#            ua.update()
-#            headers = {'User-Agent': ua.random}
-#
-#            #shuffle proxies
-#            proxies = {'http': 'http://190.64.18.177:80'} 
-#
-#            #random sleep and connect
-#            requests.get('https://translate.google.it/?hl=it')
-#
-#            url = 'https://www.fantacalcio.it/pagelle/' + str(s[1]) +'/'+ team.lower() + '/' + str(m[1])
-#            page = requests.get(url, headers=headers, proxies=proxies)
-#            soup = BeautifulSoup(page.text, 'html.parser')
-#            print(colored("connected to " + team +" at " + url, 'red'))
-#
-#            # Extract team's players and grades
-#            data_scraping(team, matchweek, season)
+seasons = get_seasons()
 
-# Data Scraping for teams, matchweeks
-for team in teams[1:]:
+# Data Scraping for seasons,matchweek,teams
+for season in seasons[1:len(seasons)-1]:
+    matchweeks = get_matchweeks()
     for matchweek in matchweeks[1:]:
+        teams = get_teams(season)
+        i = 0
+        for team in teams[1:]:
+            i = i + 1
+            if(i % 5 == 0):
+                sleep(3)
+            # rename season and matchweek
+            s = season.replace('/','-').split()
+            m = matchweek.split()
 
-        # rename season and matchweek
-        s = '2019-20'
-        m = matchweek.split()
+            # shuffle headers 
+            ua = UserAgent()
+            ua.update()
+            headers = {'User-Agent': ua.random}
 
-        # shuffle headers 
-        ua = UserAgent()
-        ua.update()
-        headers = {'User-Agent': ua.random}
+            url = 'https://www.fantacalcio.it/pagelle/' + str(s[1]) +'/'+ team.lower() + '/' + str(m[1])
+            page = requests.get(url, headers=headers)
+            soup = BeautifulSoup(page.text, 'html.parser')
+            print(colored("connected to " + team +" at " + url, 'green'))
 
-        url = 'https://www.fantacalcio.it/pagelle/' + str(s) +'/'+ team.lower() + '/' + str(m[1])
-        page = requests.get(url, headers=headers)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        print(colored("connected to " + team +" at " + url, 'red'))
-
-        # Extract team's players and grades
-        data_scraping(team, matchweek, s)
+            # Extract team's players and grades
+            data_scraping(team, matchweek, season)
